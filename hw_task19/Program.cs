f2aebfbddb28953fab6a9e2cf604624d40241fc1
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-09 12:17:56 +0300

    hw-task19

---
+++ hw_task19/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+string result = Convert.ToString(num);
+
+if(result[0] == result[4] || result[1] == result[3])
+    Console.WriteLine($"Полиндром");
+
+else
+
+Console.WriteLine("Не полиндром");
